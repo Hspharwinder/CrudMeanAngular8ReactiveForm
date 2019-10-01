e9ca41e995e04f25e1192c19f038008801842065
Harwinder <hspharwinder@gmail.com>
2019-10-01 11:01:11 +0530

    validation code

--- src/app/insert/insert.component.ts
+++ src/app/insert/insert.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, FormArray, ControlContainer } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, FormArray, ControlContainer, Validators } from '@angular/forms';
 import { CrudService } from '../Service/crud.service';
 import {Router} from '@angular/router';
 
@@ -12,20 +12,13 @@ export class InsertComponent implements OnInit {
 
   otherTextBox = false;
   formValue:FormGroup;
-  // name:string;
-  // dept:string;
-  // designation:string;
-  // email:string;
-  // password:string;
-  // gender:string;
-   selectedGames:any;
-  // other:string;
+  selectedGames:any=[];
   hobbiesList: { item_id: number; item_text: string; }[];
   dropdownSettings: { singleSelection: boolean; idField: string; textField: string; selectAllText: string; unSelectAllText: string; itemsShowLimit: number; allowSearchFilter: boolean; };
   gamesCheckBoxList: any;
-  otherGames:string;
   Dept: { id: number; name: string; desi: string[]; }[];
   designation: any;
+  validError: boolean;
 
   constructor(
     private formBuilder:FormBuilder,
@@ -33,73 +26,123 @@ export class InsertComponent implements OnInit {
     private router:Router,
     )
   {
-    this.hobbiesList = [
-      { item_id: 1, item_text: 'Action' },
-      { item_id: 2, item_text: 'Comedy' },
-      { item_id: 3, item_text: 'Drama' },
-      { item_id: 4, item_text: 'Romance' },
-      { item_id: 5, item_text: 'Dance' }
-    ];    
-    this.gamesCheckBoxList  = [
-      { item_id: 1, item_text: 'Cricket' },
-      { item_id: 2, item_text: 'Puzzle' },
-      { item_id: 3, item_text: 'Chess' },
-    ];
-    this.formValue = this.formBuilder.group({
-      name : new FormControl(),
-      dept : new FormControl(),
-      designation : new FormControl(),
-      email : new FormControl(),
-      password: new FormControl(),
-      gender : new FormControl(),
-      hobbies : new FormControl(),
-      games : this.addListControl(),
-      otherGames : new FormControl(),
-    });
-
-    this.dropdownSettings = {
-      singleSelection: false,
-      idField: 'item_id',
-      textField: 'item_text',
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
-      itemsShowLimit: 3,
-      allowSearchFilter: true
-    };
-
-
-    this.Dept = [
-      { id: 1, name: 'Development', desi: ['Manager', 'TeamLead', 'TeamMemeber']  },
-      { id: 2, name: 'Management', desi: ['Director', 'CTO', 'HR']  },
-      { id: 3, name: 'Desiging', desi: ['Sr.Designer', 'Designer', 'Jr.Designer'] },
-    ];
-
+    this.getHobbieList();
+    this.getGamesCheckBox();
+    this.formBuilderControl();    
+    this.dropdownSett();  
+    this.getDept();
   } 
 
-  // initialize all value checked = false of gamesCheckBoxList
-  addListControl(){
-    const arr = this.gamesCheckBoxList.map(element=>{
-      return this.formBuilder.control(false);
-    });   
-    return this.formBuilder.array(arr);
-  }
-
-  // property of gamesCheckBoxList it would be call each time on checkbox clicking 
-  get checkList(){
-    return <FormArray>this.formValue.get('games');
-  }
-  
-  ngOnInit(){;
-  }
-
   onSubmit(form :any){
-    //console.log(form);
-    console.log("form worked");
+    if(this.formValue.invalid){
+        this.valildateAll(form);
+    } 
+    else{
+      this.postData();
+    }      
+  }
+
+  /////*****  Validation code start *******/////  
+
+  formBuilderControl(){
+    this.formValue = this.formBuilder.group({
+      name : ['', Validators.required],
+      dept : new FormControl('',  Validators.required),
+      designation : new FormControl('', Validators.required),
+      email : new FormControl('', [Validators.required,Validators.email]),
+      password: ['', [Validators.required,Validators.minLength(5)]],
+      gender : new FormControl('', Validators.required),
+      hobbies : new FormControl('', Validators.required),
+      games : this.addListControl(),
+      otherGames : new FormControl('')
+    });
+  }
+
+
+  // incase user directly submitting form without touching control,validation apply on Submit
+  //***** start: simple validation
+  valildateAll(formValue:FormGroup){
+    Object.keys(formValue.controls).forEach(element => {
+      const control = formValue.get(element);
+      if(control instanceof FormControl){
+        control.markAllAsTouched();
+      }
+    });
+
+    // validation for checkbox
+    if(!this.typeGames.value && this.games.untouched)
+      this.validError = true;
+  }
+   //***** End: simple validation
+
+
+  /******  Start: check validation code  ********/
+  onCheckChange(event) {
+    //console.log(event);
+    this.selectedGames = [];
+    // adding selected values in selectedGames Array
+    this.checkList.controls.forEach((control, i) => {
+      if (control.value) {
+        this.selectedGames.push(this.gamesCheckBoxList[i].item_text);
+      }
+    });
     
+    //code for checkbox validation 
+    this.checkboxValidation();    
+  }  
+
+  clickTextBoxGames(e:any){
+    this.typeGames.setValue(e.target.value);
+    this.checkboxValidation();
+  }
+
+  otherChange(e:any){
+    this.otherTextBox = !this.otherTextBox;
+    console.log(e);    
+    //code for checkbox validation 
+    this.checkboxValidation();
+  } 
+
+  checkboxValidation(){
+    this.validError = this.selectedGames.length || (this.typeGames.value && this.otherTextBox)  ? false:true;
+    console.log("this.validError", this.validError);
+  } 
+  /***** End: Check validation code ********/
+
+  /////*****  End Validation code start *******///// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+  ngOnInit(){
+  }
+
+  postData(){
     // adding checkbox values in form Object
     let games = this.selectedGames || [];  // if selectedGames Empty enter empty array otherwise push error throw because there is no array name of game
-    if(this.otherTextBox && this.otherGames){
-      games.push(...this.otherGames.split(',')); 
+    let otherGames = this.typeGames.value;
+    if(this.otherTextBox && otherGames){
+      games.push(...otherGames.split(',')); 
       console.log({...this.formValue.value, games});     
     }
     else{
@@ -111,24 +154,19 @@ export class InsertComponent implements OnInit {
       this.router.navigateByUrl('/home');
     },err=>{
       console.log("error during response ",err);
-    })    
+    })  
   }
   
 
-  onCheckChange(event) {
-    //console.log(event);
-    this.selectedGames = [];
-    // adding selected values in selectedGames Array
-    this.checkList.controls.forEach((control, i) => {
-      if (control.value) {
-        this.selectedGames.push(this.gamesCheckBoxList[i].item_text);
-      }
-    });
-  }    
-  otherChange(e:any){
-    this.otherTextBox = !this.otherTextBox;
-    console.log(e);    
+  // initialize all value checked = false of gamesCheckBoxList
+  addListControl(){
+    const arr = this.gamesCheckBoxList.map(element=>{
+      return this.formBuilder.control(false);
+    });   
+    return this.formBuilder.array(arr);
   }
+
+ 
   onItemSelect(hobbies:any){
     console.log("hobbies  ",hobbies);
   }
@@ -140,6 +178,66 @@ export class InsertComponent implements OnInit {
    const value = e.target.value;
    this.designation = this.Dept.filter(x => x.name == value)[0].desi;
    console.log(this.designation)
+  }
+
+   // property of gamesCheckBoxList it would be call each time on checkbox clicking 
+   get checkList(){
+    return <FormArray>this.formValue.get('games');
+  }
+
+  get pwd(){
+    return this.formValue.get('password');
+  }
+
+  get dept(){
+    return this.formValue.get('dept');
+  }
+  get desi(){
+    return this.formValue.get('designation');
+  }
+  get games(){
+    return this.formValue.get('games');
+  }
+  get typeGames(){
+    return this.formValue.get('otherGames');
+  }
+
+  getHobbieList(){
+    this.hobbiesList = [
+      { item_id: 1, item_text: 'Action' },
+      { item_id: 2, item_text: 'Comedy' },
+      { item_id: 3, item_text: 'Drama' },
+      { item_id: 4, item_text: 'Romance' },
+      { item_id: 5, item_text: 'Dance' }
+    ];   
+  }
+
+  getGamesCheckBox(){
+    this.gamesCheckBoxList  = [
+      { item_id: 1, item_text: 'Cricket' },
+      { item_id: 2, item_text: 'Puzzle' },
+      { item_id: 3, item_text: 'Chess' },
+    ];
+  }
+
+  getDept(){
+    this.Dept = [
+      { id: 1, name: 'Development', desi: ['Manager', 'TeamLead', 'TeamMemeber']  },
+      { id: 2, name: 'Management', desi: ['Director', 'CTO', 'HR']  },
+      { id: 3, name: 'Desiging', desi: ['Sr.Designer', 'Designer', 'Jr.Designer'] },
+    ];
+  }
+
+  dropdownSett(){
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
   }
 
 }
